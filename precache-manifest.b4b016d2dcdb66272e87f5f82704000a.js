57b5571342d87b6aa07c9512c5e13e006a1abf79
Jeffrey Davis <davisj0214@yahoo.com>
2019-10-28 18:25:54 -0500

    Updates

---
+++ precache-manifest.b4b016d2dcdb66272e87f5f82704000a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bcab7bf47d682da377bce006106950",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "5356c0b5e8183cfcf946",
+    "url": "/Clicky-Game/static/css/main.3c6c68cf.chunk.css"
+  },
+  {
+    "revision": "219a77904a292db9061b",
+    "url": "/Clicky-Game/static/js/2.bc8d25d5.chunk.js"
+  },
+  {
+    "revision": "5356c0b5e8183cfcf946",
+    "url": "/Clicky-Game/static/js/main.5b8745c9.chunk.js"
+  },
+  {
+    "revision": "46ddb773f2a7377f0893",
+    "url": "/Clicky-Game/static/js/runtime-main.92642ba5.js"
+  }
+]);
